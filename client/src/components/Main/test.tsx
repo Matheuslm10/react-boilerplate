8f4a8504b84bfb744660d1ae696f166c1f99ca78
Matheus Machado <matheus_lm10@hotmail.com>
2020-08-12 23:59:49 -0400

    Primeiro uso do Styled Components

--- client/src/components/Main/test.tsx
+++ client/src/components/Main/test.tsx
@@ -9,6 +9,12 @@ describe('Main', () => {
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
