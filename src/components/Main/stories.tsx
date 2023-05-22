63758dc5be0ad666d800403188b16759002491ef
Matheus Machado <matheus_lm10@hotmail.com>
2023-05-22 07:42:05 -0400

    Downgrades storybook version to work properly

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,7 +3,17 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Basic Title',
+  description: 'Basic Description'
+}
+
+export const Default: Story = (args) => <Main {...args} />
